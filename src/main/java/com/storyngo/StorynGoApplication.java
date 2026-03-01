eb03f37e5aa160786e1b79700bb06469968438a1
EL GHAOUTI aymane <elghaoutiayman20@gmail.com>
2026-03-01 16:23:38 +0100

    chore: initial project structure from Spring Initializr

---
+++ src/main/java/com/storyngo/StorynGoApplication.java
@@ -0,0 +1,13 @@
+package com.storyngo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorynGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorynGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/storyngo/StorynGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storyngo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorynGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
